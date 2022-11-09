dd9db785945d23ff38cfb29212b267a7418e203b
alejandra.diaz <alejandra.diaz@ixcomercio.com>
2022-11-09 00:07:01 -0300

    Solucionado el error de test y sintaxis java

--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -1,13 +1,25 @@
 package com.devopsusach2020;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.devopsusach2020.model.Mundial;
+import com.devopsusach2020.rest.RestData;
 
 @SpringBootTest
 class DevOpsUsach2020ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(){
+		RestData rest = new RestData();
+		Assertions.assertNotNull(rest.getData("Chile")); 
+	}
+
+	@Test
+	void totalPais(){
+		RestData rest = new RestData();
+		Assertions.assertNotNull(rest.getTotalPais("Chile")); 
 	}
 
 }
